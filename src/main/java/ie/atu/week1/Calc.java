9e014ae036bdb82a434051b6cd4bce42d95c1e0a
Nandita <g00463584@atu.ie>
2025-10-26 15:13:27 +0000

    first part of calculating

--- src/main/java/ie/atu/week1/Calc.java
+++ src/main/java/ie/atu/week1/Calc.java
@@ -1,5 +1,23 @@
 package ie.atu.week1;
+import java.util.Scanner;
 
 public class Calc {
+public static void main(String[] args){
 
+    //Prompt the user for second number
+    // Display it to the screen
+    Scanner scan1=new Scanner(System.in);
+    //Please enter first number
+    System.out.println("Please enter a number: ");
+    double firstNum= scan1. nextDouble();
+    System.out.println("You entered "+ firstNum);
+
+    //Please enter second number
+    System.out.println("Please enter second number: ");
+    double secondNum=scan1.nextDouble();
+    System.out.println("You entered: " + secondNum);
+
+    double sum= firstNum + secondNum;
+    System.out.println("The sum is "+ sum);
+ }
 }
